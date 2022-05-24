37ee17294810de13eb636fed03c0fc102737e984
JcarlosCabello1991 <jc.cabellomangas6@gmail.com>
2022-05-24 15:54:23 +0200

    create folder funcionallity

---
+++ assets/js/main.js
@@ -0,0 +1,8 @@
+// const openDialog = document.getElementById("openDialog");
+// const dialog = document.getElementById("dialog");
+// console.log(openDialog);
+// openDialog.addEventListener("click", function(){
+//     console.log("hola");
+//     dialog.removeAttribute("close");
+//     dialog.setAttribute("open", "");
+// });
--- workshop/Directories/create-directory.php
+++ workshop/Directories/create-directory.php
@@ -1,21 +1,20 @@
 <?php
-
 /* 
 $actualDir = dirname(__FILE__);
 
 echo $actualDir;
  */
-
+  print_r("hola");
 
 
   $name = $_POST['nameFileOrDirectory'];
   $actualDir = "../root".$name;
   if (file_exists($actualDir)){
-    header("Location: ../index.php");
+    //header("Location: ../index.php");
     echo "File already exists";
   } else {
     mkdir($actualDir, 0700);
-    header("Location: ../index.php");
+    //header("Location: ../index.php");
     echo "Directory succesfully created)";
   }
 
@@ -24,3 +23,8 @@ echo $actualDir;
 
 /* mkdir("../Root", 0700); */
 ?>
+
+<!-- <dialog class='dialog' width=200px open>
+        <form action='./sendNewDirectory.php' method='POST'>
+        <input name='NameDirectory' placeholder='DirectoryName>
+        <button type='submit'>Add</button></form> -->
