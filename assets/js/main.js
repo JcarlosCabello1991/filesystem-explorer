acf2d2536d855ea2624a681c449a555373dcabb1
Javier Fernandez <dogsouldev@gmail.com>
2022-05-25 09:09:07 +0200

    Addd css things,bootstrap-icons,normalize.css

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,15 @@ let iconModify = document.querySelectorAll("[data-value]");
 
 console.log("hola");
 console.log(iconModify);
-iconModify.forEach(icon => {
-    icon.addEventListener("click", function(){
-        console.log(icon.id);
-        console.log(document.getElementById("form-modify"));
-        //let pathMod = document.getElementById("form-modify").setAttribute("action", icon.id);
-        const oldPath = document.getElementById("oldPath").setAttribute("value", icon.id);
-        console.log(pathMod+" "+oldPath);
-    })
+
+iconModify.forEach((icon) => {
+  icon.addEventListener("click", function () {
+    console.log(icon.id);
+    console.log(document.getElementById("form-modify"));
+    //let pathMod = document.getElementById("form-modify").setAttribute("action", icon.id);
+    const oldPath = document
+      .getElementById("oldPath")
+      .setAttribute("value", icon.id);
+    console.log(pathMod + " " + oldPath);
+  });
 });
